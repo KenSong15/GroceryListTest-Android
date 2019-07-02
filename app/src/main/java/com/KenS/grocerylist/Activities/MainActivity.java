ae60eb9a567e97083a09758869e90c1ede862e26
Ken <kensong96@gmail.com>
2019-07-02 14:22:37 -0400

    128D, finish popup layout

--- app/src/main/java/com/KenS/grocerylist/Activities/MainActivity.java
+++ app/src/main/java/com/KenS/grocerylist/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.KenS.grocerylist;
+package com.KenS.grocerylist.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.KenS.grocerylist.R;
 
 public class MainActivity extends AppCompatActivity {
 
